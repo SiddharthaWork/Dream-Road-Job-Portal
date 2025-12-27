cd79406cb9ad9869bdcaa5d2e53c838137a6364a
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-12-27 21:47:40 +0545

    Changes for the vercel build

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,16 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Ignore ALL build-time errors
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+  // Avoid image & font optimization crashes
   images: {
     unoptimized: true,
   },
+
+  // Disable strict & experimental failures
+  reactStrictMode: false,
+
+
 };
 
 export default nextConfig;
